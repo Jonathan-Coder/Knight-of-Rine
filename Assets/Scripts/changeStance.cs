0b3ee767a3bf20f1f5d96489c20d831ecf93c925
Jonathan Figuereo <Jfiguereo@aoiths.org>
2021-12-10 14:59:13 -0500

    Create Game

---
+++ Assets/Scripts/changeStance.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeStance : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite transition;
+    public Sprite fight;
+    public Sprite neutral;
+    private int state = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && state == 0)
+        {
+            //Enter code to change stance
+            ChangeSprite(transition);
+            StartCoroutine(change(3, fight));
+            state++;
+        } else if(Input.GetKeyDown(KeyCode.Space) && state == 1){
+            ChangeSprite(transition);
+            StartCoroutine(change(3, neutral));
+            state--;
+        }
+    }
+    void ChangeSprite(Sprite x)
+    {
+        spriteRenderer.sprite = x;
+    }
+
+    IEnumerator change(int secs, Sprite sprite)
+    {
+        yield return new WaitForSeconds(secs);
+        ChangeSprite(sprite);
+    }
+}
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    public float changeTime = 3.0f;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+        /*
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        */
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
