b1ac7220bc7926c955960d1995be389e5d1cf249
Jamaldemort <brose@aoiths.org>
2022-02-03 14:56:06 -0500

    menu worky kinda

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject scenecam;
+    public GameObject menucam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menucam.SetActive(true);
+        scenecam.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Startbutton() {
+        menucam.SetActive(false);
+        scenecam.SetActive(true);
+    }
+
+    public void Quitbutton()
+    {
+        Application.Quit();
+    }
+
+}
